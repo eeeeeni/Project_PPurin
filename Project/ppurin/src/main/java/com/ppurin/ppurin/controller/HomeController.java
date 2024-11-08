b0141bfd6a242a6d3c463e3ebe8b32b3660e15ce
eeeeeni <djfwkd35@gmail.com>
2024-11-09 01:32:25 +0900

    update1109

---
+++ Project/ppurin/src/main/java/com/ppurin/ppurin/controller/HomeController.java
@@ -0,0 +1,45 @@
+package com.ppurin.ppurin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/index")
+    public String index(Model model) {
+        // 홈 페이지로 이동
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        // about.html 파일로 이동
+        return "about";
+    }
+    
+    @GetMapping("/gallery")
+    public String gallery(Model model) {
+        // "gallery.html" 파일로 이동
+        return "gallery/gallery";
+    }
+
+    @GetMapping("/gallery/edit")
+    public String editGallery(Model model) {
+        // "edit.html" 파일로 이동
+        return "gallery/edit";
+    }
+
+    @GetMapping("/gallery/inner")
+    public String innerGallery(Model model) {
+        // "inner.html" 파일로 이동
+        return "gallery/inner";
+    }
+
+    @GetMapping("/gallery/write")
+    public String writeGallery(Model model) {
+        // "write.html" 파일로 이동
+        return "gallery/write";
+    }
+}
